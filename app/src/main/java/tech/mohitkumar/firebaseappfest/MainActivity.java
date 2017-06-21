04707d02d8c2038800fd66247de55b93d794bc88
YashishDua <yashish.dua@gmail.com>
2017-06-21 13:47:50 +0530

    Basic Add Event UI

--- app/src/main/java/tech/mohitkumar/firebaseappfest/MainActivity.java
+++ app/src/main/java/tech/mohitkumar/firebaseappfest/MainActivity.java
@@ -1,10 +1,12 @@
 package tech.mohitkumar.firebaseappfest;
 
-import android.app.Notification;
 import android.content.Context;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 
 import tech.mohitkumar.firebaseappfest.Adapter.CardPagerFragmentAdapter;
@@ -16,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
     private Button mButton;
     private ViewPager mViewPager;
 
+    private FloatingActionButton fab;
+
     private ShadowTransformer mCardShadowTransformer;
     private CardPagerFragmentAdapter mFragmentCardAdapter;
     private ShadowTransformer mFragmentCardShadowTransformer;
@@ -26,6 +30,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        fab = (FloatingActionButton)findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                View v = View.inflate(MainActivity.this,R.layout.dialog_event,null);
+                AlertDialog.Builder db = new AlertDialog.Builder(MainActivity.this);
+                db.setView(v);
+                db.create().show();
+            }
+        });
         mViewPager = (ViewPager) findViewById(R.id.viewPager);
         mViewPager.setPageMargin(-50);
 
