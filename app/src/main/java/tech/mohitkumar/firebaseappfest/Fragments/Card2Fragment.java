0d5606a61cc31e7674482ebc0aaae9009e29653f
batbrain7 <kumar.mohit983@gmail.com>
2017-06-21 12:36:46 +0530

    Added animation

---
+++ app/src/main/java/tech/mohitkumar/firebaseappfest/Fragments/Card2Fragment.java
@@ -0,0 +1,30 @@
+package tech.mohitkumar.firebaseappfest.Fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import tech.mohitkumar.firebaseappfest.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Card2Fragment extends Fragment {
+
+
+    public Card2Fragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_card2_adapter, container, false);
+    }
+
+}
--- app/src/main/java/tech/mohitkumar/firebaseappfest/MainActivity.java
+++ app/src/main/java/tech/mohitkumar/firebaseappfest/MainActivity.java
@@ -1,13 +1,50 @@
 package tech.mohitkumar.firebaseappfest;
 
+import android.app.Notification;
+import android.content.Context;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import tech.mohitkumar.firebaseappfest.Adapter.CardPagerFragmentAdapter;
+import tech.mohitkumar.firebaseappfest.CustomAnimations.ShadowTransformer;
+import tech.mohitkumar.firebaseappfest.Fragments.CardFragments;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button mButton;
+    private ViewPager mViewPager;
+
+    private ShadowTransformer mCardShadowTransformer;
+    private CardPagerFragmentAdapter mFragmentCardAdapter;
+    private ShadowTransformer mFragmentCardShadowTransformer;
+
+    private boolean mShowingFragments = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mViewPager = (ViewPager) findViewById(R.id.viewPager);
+        mViewPager.setPageMargin(-50);
+
+        mFragmentCardAdapter = new CardPagerFragmentAdapter(getSupportFragmentManager(),
+                dpToPixels(2, this));
+        mFragmentCardAdapter.addCardFragment(new CardFragments());
+        mFragmentCardAdapter.addCardFragment(new CardFragments());
+        mFragmentCardAdapter.addCardFragment(new CardFragments());
+        mFragmentCardAdapter.addCardFragment(new CardFragments());
+
+        mFragmentCardShadowTransformer = new ShadowTransformer(mViewPager, mFragmentCardAdapter);
+        mFragmentCardShadowTransformer.enableScaling();
+        mViewPager.setAdapter(mFragmentCardAdapter);
+        mViewPager.setPageTransformer(false, mFragmentCardShadowTransformer);
+        mViewPager.setOffscreenPageLimit(3);
+
+    }
+
+    public static float dpToPixels(int dp, Context context) {
+        return dp * (context.getResources().getDisplayMetrics().density);
     }
 }
